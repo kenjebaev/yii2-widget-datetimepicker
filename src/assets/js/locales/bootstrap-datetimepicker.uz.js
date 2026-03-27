4c69d8acc394b375f6d5e054ea8cdc31bacd3f4f
kenjebaev <kenjebaev.sagindik@gmail.com>
2026-03-27 14:09:44 +0500

    Add Uzbek (uz) locale for bootstrap-datetimepicker

---
+++ src/assets/js/locales/bootstrap-datetimepicker.uz.js
@@ -0,0 +1,16 @@
+/**
+ * Uzbek translation for bootstrap-datetimepicker
+ */
+;(function($){
+	$.fn.datetimepicker.dates['uz'] = {
+		days: ["Yakshanba", "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Shanba", "Yakshanba"],
+		daysShort: ["Yak", "Du", "Se", "Chor", "Pay", "Ju", "Sha", "Yak"],
+		daysMin: ["Ya", "Du", "Se", "Ch", "Pa", "Ju", "Sh", "Ya"],
+		months: ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"],
+		monthsShort: ["Yan", "Fev", "Mar", "Apr", "May", "Iyn", "Iyl", "Avg", "Sen", "Okt", "Noy", "Dek"],
+		today: "Bugun",
+		suffix: [],
+		meridiem: [],
+		weekStart: 1
+	};
+}(jQuery));
